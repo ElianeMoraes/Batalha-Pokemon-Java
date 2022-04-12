07dc90fd1b698f3f54a6799024573262d4ab43f5
ElianeMoraes <103465508+ElianeMoraes@users.noreply.github.com>
2022-04-11 21:52:58 -0300

    Salvando arquivos no git

---
+++ src/ac1_pokemon_top/AC1_Pokemon_TOP.java
@@ -0,0 +1,9 @@
+package ac1_pokemon_top;
+
+public class AC1_Pokemon_TOP {
+    public static void main(String[] args) {
+        Pokemon meuPokemon = new Pokemon("Charmander", "Fogo", 20);
+        Pokemon outroPokemom = new Pokemon("Blastoise", "Agua", 80);
+    }
+    
+}
---
+++ src/ac1_pokemon_top/Batalhas.java
@@ -0,0 +1,5 @@
+package ac1_pokemon_top;
+
+public class Batalhas {
+    
+}
